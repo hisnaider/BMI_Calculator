4372fd21d44e65d96ce672edfbbcf3da17b78fe9
Hisnaider Campello <hisnaidercampello@hotmail.com.br>
2023-04-24 16:31:30 -0300

    windows, linux and mac folder deleted

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,7 +20,6 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    print(person.gender);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
